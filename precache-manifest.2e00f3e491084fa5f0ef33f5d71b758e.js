d9eef12fbf2bc89fd7cff6e48038abe0625cd185
Pierre Lavalley <pierre.lavalley@gmail.com>
2020-05-26 16:38:29 +0200

    Updates

---
+++ precache-manifest.2e00f3e491084fa5f0ef33f5d71b758e.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "544a372d0907b4e2dfd8c80538da43cf",
+    "url": "/pirstone-website/index.html"
+  },
+  {
+    "revision": "34ef2c4f1a649a15f9a6",
+    "url": "/pirstone-website/static/css/main.06387a59.chunk.css"
+  },
+  {
+    "revision": "b61d48ac23502889ac61",
+    "url": "/pirstone-website/static/js/2.d78405e8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/pirstone-website/static/js/2.d78405e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34ef2c4f1a649a15f9a6",
+    "url": "/pirstone-website/static/js/main.a291efc2.chunk.js"
+  },
+  {
+    "revision": "ba039c66349685cbb854",
+    "url": "/pirstone-website/static/js/runtime-main.78f4b2b2.js"
+  },
+  {
+    "revision": "776d7f72efb940a55498f067a15a674d",
+    "url": "/pirstone-website/static/media/app-store-badge.776d7f72.png"
+  },
+  {
+    "revision": "d9fcc1bdeb1aaeed8bc15f6d056ee372",
+    "url": "/pirstone-website/static/media/bql-logo.d9fcc1bd.png"
+  },
+  {
+    "revision": "8b18bfe6d27adafd34a605c4def807cf",
+    "url": "/pirstone-website/static/media/bql-screen-light.8b18bfe6.png"
+  },
+  {
+    "revision": "d1990e04036eaf98c5ea126ff0379d42",
+    "url": "/pirstone-website/static/media/chrome-webstore-badge.d1990e04.png"
+  },
+  {
+    "revision": "bd6d52e0c93f7d83abeb0a03e62455a7",
+    "url": "/pirstone-website/static/media/eu-logo.bd6d52e0.png"
+  },
+  {
+    "revision": "679d8ca27cf31efa6533b80d43191a08",
+    "url": "/pirstone-website/static/media/eu-screen-light.679d8ca2.png"
+  }
+]);
