46e4fb4f02435ec63295be92f6c0a29055630876
Pierre Lavalley <pierre.lavalley@gmail.com>
2020-08-20 16:25:11 +0200

    Update EU logo / Setup React Router / EU CGU page

--- src/App.js
+++ src/App.js
@@ -1,19 +1,42 @@
 import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
 
 import Header from "./components/Header";
 import Home from "./containers/Home";
 import Footer from "./components/Footer";
-import './app.css';
+import CGUScreen from "./containers/CGUScreen";
+import "./app.css";
+
+const Shell = () => (
+  <div className="wrapper d-flex flex-column justify-content-between">
+    <Header />
+    <Home />
+    <Footer />
+  </div>
+);
 
 function App() {
   return (
-    <div className="container">
-      <div className="wrapper d-flex flex-column justify-content-between">
-        <Header />
-        <Home />
-        <Footer />
+    <Router>
+      <div className="container">
+        <Switch>
+          <Route exact path="/">
+            <Shell />
+          </Route>
+          <Route path="/eatuntil/cgu">
+            <CGUScreen />
+          </Route>
+          <Route path="*">
+            <Redirect to="/" />
+          </Route>
+        </Switch>
       </div>
-    </div>
+    </Router>
   );
 }
 
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { useTranslation } from 'react-i18next';
+import { useTranslation } from "react-i18next";
 
 import "./home.css";
 import eatuntilLogo from "./img/eu-logo.png";
